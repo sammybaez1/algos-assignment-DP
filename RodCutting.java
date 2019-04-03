69bb24e921bb3d3f567bc94ed2358dcb863c0636
sammybaez1 <44147261+sammybaez1@users.noreply.github.com>
2019-04-02 20:27:08 -0400

    Completed RodCutting
    All required tasks finished

--- RodCutting.java
+++ RodCutting.java
@@ -1,16 +1,77 @@
 /**
  * Rod cutting problem described in Chapter 15 of textbook
+ * * Author: Sammy Baez
  */
 public class RodCutting {
 
   // Do not change the parameters!
   public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
-    return 0;
+	  if (rodLength <= 0){ 
+		  return 0; 
+	  }
+	  
+	  int result = Integer.MIN_VALUE; 
+		
+		   for (int currentRodLength = 0; currentRodLength < rodLength; currentRodLength++){
+		         result = Math.max(result,lengthPrices[currentRodLength] + rodCuttingRecur(rodLength-currentRodLength-1,lengthPrices)); 
+		   }
+		   return result; 
   }
+  
+  
+  public int rodCuttingRecurMemo(int rodLength, int[] lengthPrices) {
+	  int solTable[] = new int[lengthPrices.length+1];
+	  
+	  for(int i = 0; i < solTable.length; i++){
+		  solTable[i] = Integer.MIN_VALUE;
+	  }
+	  
+	  return rodCuttingRecurMemo(rodLength,lengthPrices,solTable);
+  
+  }
+  
+  public int rodCuttingRecurMemo(int rodLength, int[] lengthPrices, int[] solTable) {
+	  if(solTable[rodLength] >= 0){
+		  return solTable[rodLength];
+	  }
+	  
+	  int result;
+	  
+	  if (rodLength <= 0){ 
+		  result = 0; 
+	  }
+	  
+	  else{
+		  result = Integer.MIN_VALUE;
+		  for (int currentRodLength = 0; currentRodLength < rodLength; currentRodLength++){
+		         result = Math.max(result,lengthPrices[currentRodLength] + rodCuttingRecurMemo(rodLength-currentRodLength-1,lengthPrices,solTable)); 
+		   }
+	  }
+	  
+	  solTable[rodLength] = result;
+	  return result;
+	  	  
+	  
+  }
+  
+  
 
   // Do not change the parameters!
   public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
-    return 0;
+	  	int solTable[] = new int[rodLength+1];
+	    solTable[0] = 0;
+	    
+	    for (int i = 1; i <= rodLength; i++){
+	      int result = Integer.MIN_VALUE;
+	      
+	      for (int j = 1; j < i; j++){
+	        result = Math.max(result, lengthPrices[j] + solTable[i-j-1]);
+	      }
+	      solTable[i] = result;
+	    }
+
+	    return solTable[rodLength];
+	  
   }
 
 
